dea1fb856a35a94b8bcc87446c64a134d09eb618
Joost Haverkort <1299167+joostit@users.noreply.github.com>
2024-07-16 14:23:55 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,50 +1,13 @@
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kdev_t.h>
-#include <linux/fs.h>
-#include <linux/err.h>
-#include <linux/device.h>
+#include<linux/module.h>
+#include<linux/init.h>
+#include<linux/kernel.h>
 
-dev_t dev = 0;
-static struct class *dev_class;
-
-static int __init hello_init(void){
-
-   /*Allocating Major number*/
-     if((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) <0){
-             pr_err("Cannot allocate major number for device\n");
-             return -1;
-     }
-     pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
-
-     /*Creating struct class*/
-     dev_class = class_create(THIS_MODULE,"etx_class");
-     if(IS_ERR(dev_class)){
-         pr_err("Cannot create the struct class for device\n");
-         goto r_class;
-     }
-
-     /*Creating device*/
-     if(IS_ERR(device_create(dev_class,NULL,dev,NULL,"etx_device"))){
-         pr_err("Cannot create the Device\n");
-         goto r_device;
-     }
-   
+static int hello_init(void){
    printk(KERN_ALERT "Hello world from kernel space! Let's do some driver things:) /\n");
    return 0;
-
-r_device:
-   class_destroy(dev_class);
-r_class:
-   unregister_chrdev_region(dev,1);
-   return -1;
 }
 
-static void __exit hello_exit(void){
-   device_destroy(dev_class,dev);
-   class_destroy(dev_class);
-   unregister_chrdev_region(dev, 1);
+static void hello_exit(void){
    printk(KERN_INFO "Goodbye cruel world from kernel space.\n");
 }
 
